de60523184ebfc7d629cc5ddcd5bdda8f535caea
60° <hatsumi.yamane.4609@gmail.com>
2019-05-22 00:09:23 +0900

    課題4 done

--- 04/mysh2.c
+++ 04/mysh2.c
@@ -1,10 +1,11 @@
 #include "mysh.h"
-#include <errno.h>	/* errno */
-#include <stdbool.h>  /* bool */
-#include <stdlib.h>   /* exit */
-#include <string.h>   /* strcmp */
+#include <errno.h>		/* errno */
+#include <signal.h>		/* kill */
+#include <stdbool.h>	/* bool */
+#include <stdlib.h>		/* exit */
+#include <string.h>		/* strcmp */
 #include <sys/wait.h> /* wait */
-#include <unistd.h>   /* fork */
+#include <unistd.h>		/* fork */
 
 /* うまく表示されない場合は，下記の 1 を 0 に切り替えて下さい */
 #if 1
@@ -13,12 +14,15 @@
 #define FIRE "FIRE\n"
 #endif
 
+int global_pid = 0;
+int stopped_pid = 0;
+
 /**
  * 引数の2文字列が等しければ真．NULLの場合は両方ともNULLであれば真．
  */
 bool streql(const char *lhs, const char *rhs) {
 	return (lhs == NULL && rhs == NULL) ||
-		   (lhs != NULL && rhs != NULL && strcmp(lhs, rhs) == 0);
+				 (lhs != NULL && rhs != NULL && strcmp(lhs, rhs) == 0);
 }
 
 void wait_children(int pid, int options) {
@@ -37,7 +41,7 @@ void wait_children(int pid, int options) {
 	} else {
 		// 子プロセスの状態が変化している
 		LOG("Waited: pid=%d, status=%d, exit_status=%d", waited_pid, status,
-			WEXITSTATUS(status));
+				WEXITSTATUS(status));
 		if (WIFEXITED(status)) {
 			write(STDERR_FILENO, FIRE, 5);
 		}
@@ -50,10 +54,9 @@ void sigchld_handler(int sig) {
 
 void sigtstp_handler(int sig) {
 	// ここのpidを止めたい子プロセスにしたい
-	kill(pid, sig);
-}
-void set_sigstp_handler(int sig, int pid) {
-	signal(SIGTSTP, sigtstp_handler);
+	stopped_pid = global_pid;
+	LOG("sigstp pid: %d", stopped_pid);
+	kill(stopped_pid, sig);
 }
 
 int invoke_node(node_t *node) {
@@ -63,6 +66,20 @@ int invoke_node(node_t *node) {
 	/* & 付きで起動しているか否か */
 	if (node->async) {
 		LOG("{&} found: async execution required");
+	}
+
+	// 停止中のコマンドを同期実行として再開
+	if (strcmp(node->argv[0], "fg") == 0) {
+		kill(stopped_pid, SIGCONT);
+		wait_children(stopped_pid, WUNTRACED);
+		return 0;
+	}
+
+	// 停止中のコマンドを非同期実行として再開
+	if (strcmp(node->argv[0], "bg") == 0) {
+		kill(stopped_pid, SIGCONT);
+		signal(SIGCHLD, sigchld_handler);
+		return 0;
 	}
 
 	/* 子プロセスの生成 */
@@ -85,6 +102,9 @@ int invoke_node(node_t *node) {
 		if (setpgid(pid, pid) == -1) {
 			// エラーの場合
 			PERROR_DIE(NULL);
+		} else {
+			global_pid = pid;
+			LOG("global_pid: %d", global_pid);
 		}
 
 		/* 生成した子プロセスを待機 */
@@ -93,7 +113,7 @@ int invoke_node(node_t *node) {
 			signal(SIGCHLD, sigchld_handler);
 		} else {
 			// 作った子プロセスを待つ
-			wait_children(pid, 0);
+			wait_children(pid, WUNTRACED);
 		}
 
 		return pid;
@@ -103,4 +123,8 @@ int invoke_node(node_t *node) {
 /* A hook point to initialize shell */
 void init_shell(void) {
 	LOG("Initializing mysh2...");
+
+	LOG("setting SIGSTP handler...");
+	signal(SIGTSTP, sigtstp_handler);
+	LOG("done!");
 }
