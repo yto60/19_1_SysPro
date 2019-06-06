aab7beb7bde74fa47ed63f3a1ca1c24d254f899d
60° <hatsumi.yamane.4609@gmail.com>
2019-06-06 11:50:36 +0900

    課題6-1

--- 06/inst0.c
+++ 06/inst0.c
@@ -1,24 +1,25 @@
 #include <dlfcn.h> /* dladdr */
 #include <stdio.h> /* printf */
 
-__attribute__((no_instrument_function))
-const char *addr2name(void* address) {
-    Dl_info dli;
-    if (dladdr(address, &dli) != 0) {
-        return dli.dli_sname;
-    } else {
-        return NULL;
-    }
+__attribute__((no_instrument_function)) const char *addr2name(void *address) {
+	Dl_info dli;
+	if (dladdr(address, &dli) != 0) {
+		return dli.dli_sname;
+	} else {
+		return NULL;
+	}
 }
 
-__attribute__((no_instrument_function))
-void __cyg_profile_func_enter(void *addr, void *site_call) {
-    /* Not Yet Implemented */
+__attribute__((no_instrument_function)) void
+__cyg_profile_func_enter(void *addr, void *site_call) {
+	/* Not Yet Implemented */
+	printf(">>> %s (%p)\n", addr2name(addr), addr);
 }
 
-__attribute__((no_instrument_function))
-void __cyg_profile_func_exit(void *addr, void *site_call) {
-    /* Not Yet Implemented */
+__attribute__((no_instrument_function)) void
+__cyg_profile_func_exit(void *addr, void *site_call) {
+	/* Not Yet Implemented */
+	printf("<<< %s (%p)\n", addr2name(addr), addr);
 }
 
 void foo(void);
@@ -27,27 +28,26 @@ void baz(void);
 void qux(void);
 
 int main(void) {
-    printf("main: %p\n", main);
-    foo();
-    foo();
-    return 0;
+	printf("main: %p\n", main);
+	foo();
+	foo();
+	return 0;
 }
 
 void foo(void) {
-    printf("main -> foo: %p\n", foo);
-    bar();
-    baz();
+	printf("main -> foo: %p\n", foo);
+	bar();
+	baz();
 }
 
 void bar(void) {
-    printf("main -> foo -> bar: %p\n", bar);
+	printf("main -> foo -> bar: %p\n", bar);
 }
 
 void baz(void) {
-    printf("main -> foo -> baz: %p\n", baz);
+	printf("main -> foo -> baz: %p\n", baz);
 }
 
 void qux(void) {
-    printf("qux?\n");
+	printf("qux?\n");
 }
-
